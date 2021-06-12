afceaf5ecfb0af2190f0b8769ebffc0cdd0f27fc
Jefte Keller <kellergnr@gmail.com>
2021-06-11 21:22:15 -0300

    refactor: extracted logic to the serializer

--- levels/serializers.py
+++ levels/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 # from rest_framework.validators import UniqueValidator
 
-# from .models import Level
+from .models import Level
 
 
-class LevelSerializer(serializers.Serializer):
+class BaseLevelSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
 
     name = serializers.CharField(
@@ -12,8 +12,34 @@ class LevelSerializer(serializers.Serializer):
         # validators=[UniqueValidator(queryset=Level.objects.all())])
     )
     fill_priority = serializers.IntegerField()
+
+
+class LevelInputSerializer(BaseLevelSerializer):
     motorcycle_spaces = serializers.IntegerField()
     car_spaces = serializers.IntegerField()
+
+
+class LevelOutputSerializer(BaseLevelSerializer):
+    motorcycle_spaces = serializers.IntegerField(write_only=True)
+    car_spaces = serializers.IntegerField(write_only=True)
+
+    available_spaces = serializers.SerializerMethodField(
+        'get_available_spaces', read_only=True)
+
+    def get_available_spaces(self, level):
+        queryset = Level.objects.get(id=level.id)
+
+        motorcycle_space_used = queryset.level_spaces.filter(
+            variety='motorcycle').count()
+        car_space_used = queryset.level_spaces.filter(variety='car').count()
+
+        available_motorcycle_spaces = queryset.motorcycle_spaces - motorcycle_space_used
+        available_car_spaces = queryset.car_spaces - car_space_used
+
+        return {
+            'available_motorcycle_spaces': available_motorcycle_spaces,
+            'available_car_spaces': available_car_spaces,
+        }
 
 
 class LevelSpaceSerializer(serializers.Serializer):
--- levels/views.py
+++ levels/views.py
@@ -6,7 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Level
-from .serializers import LevelSerializer
+from .serializers import LevelInputSerializer, LevelOutputSerializer
 
 
 class CreateListLevelView(APIView):
@@ -14,43 +14,19 @@ class CreateListLevelView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def post(self, request):
-        input_serializer = LevelSerializer(data=request.data)
+        input_serializer = LevelInputSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
 
         new_level = Level.objects.create(**input_serializer.data)
 
-        output_serializer = LevelSerializer(new_level)
+        output_serializer = LevelOutputSerializer(new_level)
         output_data = output_serializer.data
-
-        output_data['available_spaces'] = {
-            'available_motorcycle_spaces': output_data['motorcycle_spaces'],
-            'available_car_spaces': output_data['car_spaces']
-        }
-        del output_data['motorcycle_spaces']
-        del output_data['car_spaces']
 
         return Response(output_data, status=status.HTTP_201_CREATED)
 
     def get(self, request):
         queryset_levels = Level.objects.all()
 
-        output_data = []
-        for queryset in queryset_levels:
-            level = LevelSerializer(queryset).data
-
-            car_space_used = queryset.level_spaces.filter(
-                variety='car').count()
-            motorcycle_space_used = queryset.level_spaces.filter(
-                variety='motorcycle').count()
-
-            level['available_spaces'] = {
-                'available_motorcycle_spaces':
-                level['motorcycle_spaces'] - motorcycle_space_used,
-                'available_car_spaces': level['car_spaces'] - car_space_used
-            }
-            del level['motorcycle_spaces']
-            del level['car_spaces']
-
-            output_data.append(level)
+        output_data = LevelOutputSerializer(queryset_levels, many=True).data
 
         return Response(output_data, status=status.HTTP_200_OK)
